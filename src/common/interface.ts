9153d5d03e7eaf1754714be969250bbebf7eed01
Artyom Kayun <kayun.artem@gmail.com>
2016-11-02 00:51:12 +0600

    add linkedList interfaces

---
+++ src/common/interface.ts
@@ -0,0 +1,21 @@
+import { Type } from 'common/type';
+
+export namespace Interface {
+
+  export interface ILinkedListNode<T> {
+    value: T;
+    next: ILinkedListNode<T> | null;
+    prev?: ILinkedListNode<T> | null;
+  };
+
+  export interface ILinkedList<T> extends Iterable<T> {
+    add(value: T): void;
+    remove(value: T): boolean;
+    contains(value: T): boolean;
+    clear(): void;
+    copyToArray(targetArray: T[], index?: number): T[];
+    readonly length: number
+  };
+}
+
+
